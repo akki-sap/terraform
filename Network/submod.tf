5f7d46a0974bde0641c394a4508dde932a85da51
akki-sap <85238674+akki-sap@users.noreply.github.com>
2021-06-18 16:47:02 +0530

    Update submod.tf

--- Network/submod.tf
+++ Network/submod.tf
@@ -1,5 +1,5 @@
 resource "google_compute_instance" "default" {							
-  name         = "test1"							
+  name         = "test10"							
   machine_type = "e2-medium"							
   zone         = "us-central1-a"							
   boot_disk {							
